d443a469368ff6eb3f0d07b3b8aa1abf98c880e0
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2019-09-26 16:50:20 -0300

    componentizing project/3 parts

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import Timeline from './components/Timeline';
+
+
+function App() {
+  return (
+    <div id="root">
+      <div className="main">
+        <Header/>
+        <Timeline/>
+      </div>
+    </div>
+     
+  );
+}
+
+export default App;
