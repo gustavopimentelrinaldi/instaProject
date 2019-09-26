cd0bcbc673a2027cb85834390b519b2eed614265
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2019-09-26 16:56:45 -0300

    componentizing a bit more

--- src/components/Foto.js
+++ src/components/Foto.js
@@ -1,6 +1,59 @@
 import React, { Component } from 'react';
 
-class Header extends Component {
+class FotoAtualizacoes extends Component{
+  render(){
+    return(
+      <section className="fotoAtualizacoes">
+        <a href="#" className="fotoAtualizacoes-like">Likar</a>
+        <form className="fotoAtualizacoes-form">
+          <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo"/>
+          <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit"/>
+        </form>
+      </section>         
+    )
+  }
+}
+
+class FotoInfo extends Component{
+  render(){
+    return(
+      <div className="foto-in fo">
+        <div className="foto-info-likes">
+          <a href="#">
+            alots_ssa
+          </a>
+          ,
+          <a href="#">
+            rafael_rollo
+          </a>
+          curtiram
+        </div>
+
+        <p className="foto-info-legenda">
+          <a className="foto-info-autor">autor </a>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, illo?
+        </p>
+
+        <ul className="foto-info-comentarios">
+          <li className="comentario">
+            <a className="foto-info-autor">seguidor </a>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ad, molestiae.
+          </li>
+          <li className="comentario">
+            <a className="foto-info-autor">seguidor </a>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt cumque earum molestias voluptatem modi nihil sit magnam ratione eveniet distinctio magni error asperiores dignissimos tempora expedita, laborum ex soluta hic maiores veritatis deserunt.
+          </li>
+          <li className="comentario">
+            <a className="foto-info-autor">seguidor </a>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laudantium quae ab fuga odio delectus maiores voluptatibus sit commodi quidem.
+          </li>
+        </ul>
+      </div>   
+    )
+  }
+}
+
+class FotoHeader extends Component {
     render(){
         return (
           <header className="foto-header">
@@ -22,41 +75,10 @@ export default class Foto extends Component{
   render(){
     return(
       <div className="foto">
-        <Header/>
+        <FotoHeader/>
         <img alt="foto" className="foto-src" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSR0r5miP78KivBywspTC3D7XBrpyJO2nUq-HyRE_HilitHR0kO"/>
-
-        <div className="foto-in fo">
-          <div className="foto-info-likes">
-            <a href="#">
-              alots_ssa
-            </a>
-            ,
-            <a href="#">
-              rafael_rollo
-            </a>
-            curtiram
-          </div>
-
-          <p className="foto-info-legenda">
-            <a className="foto-info-autor">autor </a>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, illo?
-          </p>
-
-          <ul className="foto-info-comentarios">
-            <li className="comentario">
-              <a className="foto-info-autor">seguidor </a>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ad, molestiae.
-            </li>
-            <li className="comentario">
-              <a className="foto-info-autor">seguidor </a>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt cumque earum molestias voluptatem modi nihil sit magnam ratione eveniet distinctio magni error asperiores dignissimos tempora expedita, laborum ex soluta hic maiores veritatis deserunt.
-            </li>
-            <li className="comentario">
-              <a className="foto-info-autor">seguidor </a>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laudantium quae ab fuga odio delectus maiores voluptatibus sit commodi quidem.
-            </li>
-          </ul>
-        </div>   
+        <FotoInfo/>
+        <FotoAtualizacoes/>
       </div>
     )
   }
