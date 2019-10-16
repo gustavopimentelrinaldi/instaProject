5de7e19eb5a5e9c955c1df96f7404039de6715f7
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2019-10-16 17:14:30 -0300

    getting props from api

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -9,7 +9,7 @@ export default class Timeline extends Component {
     }
 
     componentDidMount(){
-      fetch('http://localhost:8080/api/public/fotos/alots')
+      fetch('https://instalura-api.herokuapp.com/api/public/fotos/alots')
       .then(response => response.json())
       .then(fotos => {
         this.setState({fotos: fotos});
@@ -22,7 +22,7 @@ export default class Timeline extends Component {
           {
             this.state.fotos.map(foto => <FotoItem foto={foto}/>)
           }
-          <Foto/>
+          <FotoItem/>
         </div>            
         );
     }
